97193d6a603a31a8170e70d57bf497853c81b294
shyzh_li@163.com <shyzh_li@163.com>
2017-12-27 18:52:14 +0800

    Initial Commit

---
+++ WavAmrConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WavAmrConverter
+//
+//  Created by liyang on 2017/12/27.
+//  Copyright © 2017年 dashang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WavAmrConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WavAmrConverter
+//
+//  Created by liyang on 2017/12/27.
+//  Copyright © 2017年 dashang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
